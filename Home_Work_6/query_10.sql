a56b5a6824e049e1827f3b9b666de861ff3c5a63
egorshanin21 <egorshanin21@gmail.com>
2023-05-10 13:50:53 +0300

    Home_Work_6
    SQL

---
+++ Home_Work_6/query_10.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT d.name
+FROM disciplines d
+JOIN grades g ON g.discipline_id = d.id
+JOIN students s ON s.id = g.student_id
+JOIN teachers t ON t.id = d.teacher_id
+WHERE s.fullname = 'James Watson' AND t.fullname = 'Roger Hunt';
---
+++ Home_Work_6/query_9.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT students.fullname, disciplines.name
+FROM students
+JOIN grades ON students.id = grades.student_id
+JOIN disciplines ON grades.discipline_id = disciplines.id
+WHERE students.fullname = 'Kimberly Wyatt';
